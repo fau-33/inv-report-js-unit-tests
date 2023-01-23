66c6bc5addbf3859b54c33448eaca49cc3546a9d
Flávio Félix <fau_felix30@hotmail.com>
2023-01-23 14:59:20 -0300

    Feito o requisito 1 average

--- src/average.js
+++ src/average.js
@@ -11,8 +11,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // adicione seu código aqui
+const average = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
+  let sum = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    sum += Math.round(array[index]);
+  }
+  return Math.round(sum / array.length);  
 };
 
 module.exports = average;
